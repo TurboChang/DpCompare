4ef5013022660ade55aff58438865953489ebdad
clx@datapipeline.com <clx@datapipeline.com>
2021-08-17 17:51:01 +0800

    2021-08-17 18:00

--- core/test.py
+++ core/test.py
@@ -5,19 +5,19 @@ from kafka import KafkaConsumer, TopicPartition
 
 size = 1000000
 
-consumer1 = KafkaConsumer(bootstrap_servers='localhost:9092')
+consumer1 = KafkaConsumer(bootstrap_servers='59.110.219.31:9092')
 def kafka_python_consumer1():
     consumer1.subscribe(['topic1'])
     for msg in consumer1:
       print(msg)
 
-consumer2 = KafkaConsumer(bootstrap_servers='localhost:9092')
+consumer2 = KafkaConsumer(bootstrap_servers='59.110.219.31:9092')
 def kafka_python_consumer2():
     consumer2.assign([TopicPartition('topic1', 1), TopicPartition('topic2', 1)])
     for msg in consumer2:
         print(msg)
 
-consumer3 = KafkaConsumer(bootstrap_servers='localhost:9092')
+consumer3 = KafkaConsumer(bootstrap_servers='59.110.219.31:9092')
 def kafka_python_consumer3():
     partition = TopicPartition('topic3', 0)
     consumer3.assign([partition])
