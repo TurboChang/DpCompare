2c2d31dfe2ee02700ab9a997046494abd7fbef79
clx@datapipeline.com <clx@datapipeline.com>
2021-09-08 18:00:08 +0800

    2021-09-08 18:00

--- mssql/multi_processes.py
+++ mssql/multi_processes.py
@@ -40,9 +40,10 @@ class Base:
     """
 
     def __init__(self, proc, batch):
+        self.proc = proc
         self.batch = batch
         self.pool = self.create_pool()
-        self.executor = ThreadPoolExecutor(max_workers=proc)
+        self.executor = ThreadPoolExecutor(max_workers=self.proc)
 
     def create_pool(self):
         """
@@ -53,7 +54,7 @@ class Base:
                         maxconnections=0,  # 连接池允许的最大连接数，0和None表示不限制连接数
                         mincached=4,  # 初始化时，链接池中至少创建的空闲的链接，0表示不创建
                         maxcached=0,  # 链接池中最多闲置的链接，0和None不限制
-                        maxusage=1,  # 一个链接最多被重复使用的次数，None表示无限制
+                        maxusage=None,  # 一个链接最多被重复使用的次数，None表示无限制
                         blocking=True,  # 连接池中如果没有可用连接后，是否阻塞等待。True，等待；False，不等待然后报错
                         host="60.205.227.46",  # 此处必须是是127.0.0.1
                         port=1433,
@@ -80,7 +81,14 @@ class Base:
             traceback.print_exc()
 
     # 插入数据
-    # @db_call
+    # def insertdata(self, data):
+    #     values_list = []
+    #     sql = "insert into test (col1, col2, col3, col4, col5, col6, col7, col8, col9) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    #     args = tuple([data for i in range(9) if id and data])
+    #     [values_list.append(args) for i in range(self.batch) if args]
+    #     # self.save_mysql(sql, values_list)
+    #     [self.save_mysql(sql, values_list) for i in range(self.proc)]
+
     def insertdata(self, data):
         values_list = []
         sql = "insert into test (col1, col2, col3, col4, col5, col6, col7, col8, col9) values (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
@@ -92,12 +100,15 @@ class Base:
     def find_all_done(self):
         tasks = []
         datas = []
-        tasks.append(self.executor.submit(self.insertdata, "xxx"))  # submit函数来提交线程需要执行的任务（函数名和参数）到线程池中，不阻塞
-        [datas.append(future.result()) for future in as_completed(tasks)]  # as_completed()是ThreadPoolExecutor中的方法，用于取出所有任务的结果
+
+        # submit函数来提交线程需要执行的任务（函数名和参数）到线程池中，不阻塞
+        [tasks.append(self.executor.submit(self.insertdata, "xxx")) for i in range(self.proc) if self.proc]
+        # as_completed()是ThreadPoolExecutor中的方法，用于取出所有任务的结果
+        [datas.append(future.result()) for future in as_completed(tasks)]
 
 
 if __name__ == '__main__':
-    f = Base(4, 10000)
+    f = Base(4, 2500)
     f.find_all_done()
     # f.insertdata("xxx")
 
